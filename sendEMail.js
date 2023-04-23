dd8581ac631eac1c869369336c1280973ba90292
Ramic <Amra.Ramic@thi.de>
2023-04-23 18:36:37 +0200

    changes

--- sendEMail.js
+++ sendEMail.js
@@ -1,7 +1,11 @@
 function sendEmail() {
-    email = document.getElementById("email")
-    text = document.getElementById("message")
-    full_name = document.getElementById("name")
+    console.log("drinnen");
+    email = document.getElementById("email");
+    text = document.getElementById("message");
+
+    full_name = document.getElementById("name");
+    console.log(email + " - " + text + " - " + full_name);
+
     Email.send({
       To : 'ra-automobile@hotmail.com',
       From : email,
